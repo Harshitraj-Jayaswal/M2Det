3d08cc5f6819879ce847afd8fb65a0f8be36198e
Harshitraj Jayaswal <81687053+Harshitraj-Jayaswal@users.noreply.github.com>
2022-07-05 04:47:25 +0530

    Update test.py

--- test.py
+++ test.py
@@ -39,33 +39,38 @@ with torch.no_grad():
 
 def test_net(save_folder, net, detector, cuda, testset, transform, max_per_image=300, thresh=0.005):
     if not os.path.exists(save_folder):
-        os.mkdir(save_folder)
+        os.mkdir(save_folder) #making eval/COCO folder if not made
 
-    num_images = len(testset)
+    num_images = len(testset) #Number of images
     print_info('=> Total {} images to test.'.format(num_images),['yellow','bold'])
-    num_classes = cfg.model.m2det_config.num_classes
-    all_boxes = [[[] for _ in range(num_images)] for _ in range(num_classes)]
+    num_classes = cfg.model.m2det_config.num_classes #number of classes
+    all_boxes = [[[] for _ in range(num_images)] for _ in range(num_classes)] # a list of size (num_classes,num_images) where each element is an empty list->[]
 
     _t = {'im_detect': Timer(), 'misc': Timer()}
-    det_file = os.path.join(save_folder, 'detections.pkl')
+    det_file = os.path.join(save_folder, 'detections.pkl') #det_file=eval/COCO/detection.pkl
     tot_detect_time, tot_nms_time = 0, 0
     print_info('Begin to evaluate',['yellow','bold'])
     for i in tqdm(range(num_images)):
-        img = testset.pull_image(i)
+        img = testset.pull_image(i) #taking one image
         # step1: CNN detection
-        _t['im_detect'].tic()
-        boxes, scores = image_forward(img, net, cuda, priors, detector, transform)
-        detect_time = _t['im_detect'].toc()
+        _t['im_detect'].tic() #starting the time
+
+        boxes, scores = image_forward(img, net, cuda, priors, detector, transform) 
+        '''Making prediction which includes the process: 
+        image preprocessing->inputing image to model->getting predictions->passing through detector() function [which decodes the location preds(boundry box coordinates) and apply nms]->getting o/p i.e. boxes and scores'''
+
+        detect_time = _t['im_detect'].toc() #return total time for prediction for one image
+         
         # step2: Post-process: NMS
-        _t['misc'].tic()
-        nms_process(num_classes, i, scores, boxes, cfg, thresh, all_boxes, max_per_image)
-        nms_time = _t['misc'].toc()
+        _t['misc'].tic() #Starting the timer
+        nms_process(num_classes, i, scores, boxes, cfg, thresh, all_boxes, max_per_image) #applying nms for that one image (after nms all the coordinates and scores in all_boxes for every image after the loop ends)
+        nms_time = _t['misc'].toc() #return the total time for nms for that one image
 
         tot_detect_time += detect_time if i > 0 else 0
         tot_nms_time += nms_time if i > 0 else 0
 
     with open(det_file, 'wb') as f:
-        pickle.dump(all_boxes, f, pickle.HIGHEST_PROTOCOL)
+        pickle.dump(all_boxes, f, pickle.HIGHEST_PROTOCOL) #saving the predictions
     print_info('===> Evaluating detections',['yellow','bold'])
     testset.evaluate_detections(all_boxes, save_folder)
     print_info('Detect time per image: {:.3f}s'.format(tot_detect_time / (num_images-1)))
